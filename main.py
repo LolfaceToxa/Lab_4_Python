4c11c117260e2e7aaed984cd83f9e27fbf25226a
Anton <antondushin1337@yandex.ru>
2021-09-28 00:02:03 +0300

    new version

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from Worker import Worker
 from Auto import Auto
 from Boss import Boss
 from Machine import Machine
+import unittest
 
 
-def enter_int(): #проверка ввода натуральных чисел
+def enter_int():  # проверка ввода натуральных чисел
     while True:
         try:
             x = int(input())
@@ -54,49 +55,47 @@ def enter_bool():
             print("Ошибка ввода")
     return answer
 
-
 # 1 - индивидуальное задание
-human1 = Worker()
-
-
-print("Ввод данных обычного сотрудника\n_____________________________________________")
-print("Введите фамилию сотрудника: ")
-sur = enter()
-print("Введите зарплату сотрудника: ")
-sal = enter_int()
-print("Введите должность сотрудника: ")
-stat = enter()
-print("Введите ОС аппаратуры сотрудника")
-system = enter()
-print("Введите ID аппаратуры сотрудника")
-ID = enter_int()
-
-human2 = Worker(sur, sal, stat, system, ID)
-
-print("Ввод данных сотрудника(Worker)\n_____________________________________________")
-print("Введите фамилию сотрудника: ")
-sur = enter()
-print("Введите зарплату сотрудника: ")
-sal = enter_int()
-print("Введите должность сотрудника: ")
-stat = enter()
-print("Введите ОС аппаратуры сотрудника")
-system = enter()
-print("Введите ID аппаратуры сотрудника")
-ID = enter_int()
-
-human3 = Worker(sur, sal, stat, system, ID)
-
-
-print(human1.__str__())
-print(human2.__str__())
-print(human3.__str__())
-
-humanity = (human1, human2, human3)
-for human in humanity:
-    human.engineer(human.surname, human.status)
-    human.salary_up(human.salary)
-
+# human1 = Worker()
+#
+#
+# print("Ввод данных обычного сотрудника\n_____________________________________________")
+# print("Введите фамилию сотрудника: ")
+# sur = enter()
+# print("Введите зарплату сотрудника: ")
+# sal = enter_int()
+# print("Введите должность сотрудника: ")
+# stat = enter()
+# print("Введите ОС аппаратуры сотрудника")
+# system = enter()
+# print("Введите ID аппаратуры сотрудника")
+# ID = enter_int()
+#
+# human2 = Worker(sur, sal, stat, system, ID)
+#
+# print("Ввод данных сотрудника(Worker)\n_____________________________________________")
+# print("Введите фамилию сотрудника: ")
+# sur = enter()
+# print("Введите зарплату сотрудника: ")
+# sal = enter_int()
+# print("Введите должность сотрудника: ")
+# stat = enter()
+# print("Введите ОС аппаратуры сотрудника")
+# system = enter()
+# print("Введите ID аппаратуры сотрудника")
+# ID = enter_int()
+#
+# human3 = Worker(sur, sal, stat, system, ID)
+#
+#
+# print(human1)
+# print(human2)
+# print(human3)
+#
+# humanity = (human1, human2, human3)
+# for human in humanity:
+#     human.engineer()
+#     human.salary_up()
 # 2 - система классов
 # print("Ввод данных сотрудника(Worker)\n_____________________________________________")
 # print("Введите фамилию сотрудника: ")
@@ -114,8 +113,8 @@ for human in humanity:
 # print(work1.__str__())
 # print("_____________________________________________")
 #
-# work1.salary_up(work1.salary)
-# work1.engineer(work1.surname, work1.status)
+# work1.salary_up()
+# work1.engineer()
 # print(work1.__str__())
 #
 # print("Ввод данных начальника(Boss)\n_____________________________________________")
@@ -128,7 +127,7 @@ for human in humanity:
 # print("Введите ОС аппаратуры сотрудника: ")
 # system = enter()
 # print("Введите ID аппаратуры сотрудника: ")
-# system = enter_int()
+# ID = enter_int()
 # print("Введите количество подчинённых сотрудника: ")
 # numOE = enter_int()
 # print("Введите марку автомобиля начальника:")
@@ -143,14 +142,17 @@ for human in humanity:
 # print(work2.__str__())
 #
 # print("_____________________________________________")
-# work2.rank_up(work2.status)
-# work2.try_money(work2.salary)
-# work2.drive(work2.fuel, work2.surname)
-# work2.refuel(work2.fuel)
-# work2.drive(work2.fuel, work2.surname)
+#
+# work2.rank_up()
+# work2.try_money()
+# work2.drive()
+# work2.refuel()
+# work2.drive()
+#
 # print(work2.__str__())
+#
 # print("_____________________________________________")
-#3 ввод/вывод бинарников
+# 3 ввод/вывод бинарников
 # import os
 # import pickle
 #
@@ -175,8 +177,6 @@ for human in humanity:
 #                 print(f"{t} был создан")
 #     except IOError:
 #         print("Ошибка чтения файла")
-#     except:
-#         print("Что-то пошло не так")
 #
 # def save(t, i):
 #     try:
@@ -191,8 +191,6 @@ for human in humanity:
 #         print(n)
 #     except IOError:
 #         print("Ошибка чтения файла")
-#     except:
-#         print("Что-то пошло не так")
 #
 #
 # lst = {'machine': [], 'worker': [], 'auto': [], 'boss': []}
@@ -208,3 +206,22 @@ for human in humanity:
 # bin2 = Worker()
 # lst['worker'].append(bin2)
 # save(worker1, 'worker')
+
+# 4 тесты
+
+class Test(unittest.TestCase):
+
+    example = Auto("красный", "Renault", 2)
+
+    def test_color(self):
+        self.assertTrue(self.example.color, msg="Название не может быть пустой строкой")
+
+    def test_mark(self):
+        self.assertTrue(self.example.mark, msg="Название не может быть пустой строкой")
+
+    def test_fuel(self):
+        self.assertTrue(isinstance(self.example.fuel, bool), msg="Некорректное значение топлива")
+
+
+if __name__ == "__main__":
+    unittest.main()
